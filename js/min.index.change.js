5d88e55330358cc2824c4224c8f4762b50788568
maynardrodgers <maynardrodgers@gmail.com>
2022-03-19 07:10:47 +0700

    update

--- js/min.index.change.js
+++ js/min.index.change.js
@@ -134,3 +134,21 @@ roadmapEffect = function() {
     }
 }
   
+getWidth = function() {
+    return Math.max(
+      document.body.scrollWidth,
+      document.documentElement.scrollWidth,
+      document.body.offsetWidth,
+      document.documentElement.offsetWidth,
+      document.documentElement.clientWidth
+    );
+  }
+  getHeight = function() {
+    return Math.max(
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.offsetHeight,
+      document.documentElement.clientHeight
+    );
+  }
